391f67d3e7548f5a9baf89cdd82bc8a0791b61e4
dhwpdnr <ink1237@gmail.com>
2023-01-03 10:01:04 +0900

    coding test practice

---
+++ programmers/230103_2.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for i in range(n):
+    print("*" * (i + 1))
+
+
+# 프로그래머스 연습 문제
---
+++ programmers/230104_4.py
@@ -0,0 +1,8 @@
+def solution(my_string, n):
+    answer = ''
+    for i in my_string:
+        answer += i * n
+
+    return answer
+
+    # 프로그래머스 연습 문제
