ea4a994865669e6ee5466c34eb98d2abc4245b48
dhwpdnr <ink1237@gmail.com>
2023-01-13 09:58:03 +0900

    230113
    프로그래머스 연습문제 모음제거, 문자열 정렬하기, 숨어있는 숫자의 덧셈

---
+++ programmers/230113_1.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = my_string
+    a = answer.replace("a", "")
+    e = a.replace("e", "")
+    i = e.replace("i", "")
+    o = i.replace("o", "")
+    u = o.replace("u", "")
+    return u
+
+    # 프로그래머스 연습문제 모음제거
---
+++ programmers/230113_2.py
@@ -0,0 +1,27 @@
+def solution(my_string):
+    answer = []
+    for i in my_string :
+        if i == "0" :
+            answer.append(int(i))
+        elif i == "1":
+            answer.append(int(i))
+        elif i == "2":
+            answer.append(int(i))
+        elif i == "3":
+            answer.append(int(i))
+        elif i == "4":
+            answer.append(int(i))
+        elif i == "5":
+            answer.append(int(i))
+        elif i == "6":
+            answer.append(int(i))
+        elif i == "7":
+            answer.append(int(i))
+        elif i == "8":
+            answer.append(int(i))
+        elif i == "9":
+            answer.append(int(i))
+    answer.sort()
+    return answer
+
+    # 프로그래머스 연습문제 문자열 정렬하기
