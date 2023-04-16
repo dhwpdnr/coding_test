581dd8709076367f3f4c1315bad44c9238a45ffc
dhwpdnr <ink1237@gmail.com>
2023-04-16 20:33:45 +0900

    230416
    백준 문제 풀이

---
+++ baekjoon/2304/2495.py
@@ -0,0 +1,17 @@
+answer = []
+
+for _ in range(3):
+    s = str(input())
+    mymax = 1
+    cnt = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            cnt += 1
+        else:
+            mymax = max(cnt, mymax)
+            cnt = 1
+    mymax = max(cnt, mymax)
+    answer.append(mymax)
+
+for i in answer:
+    print(i)
--- baekjoon/2304/5523.py
+++ baekjoon/2304/5523.py
@@ -1,8 +1,10 @@
+from sys import stdin
+
 case = int(input())
 a_win = 0
 b_win = 0
 for _ in range(case):
-    a_score, b_score = map(int, input().split())
+    a_score, b_score = map(int, stdin.readline().split())
     if a_score > b_score:
         a_win += 1
     elif a_score < b_score:
