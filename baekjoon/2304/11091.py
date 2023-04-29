77d78e229927e28e191aa9c01e81052a25e229c1
dhwpdnr <ink1237@gmail.com>
2023-04-29 20:25:51 +0900

    230429
    백준 문제 풀이

---
+++ baekjoon/2304/11091.py
@@ -0,0 +1,13 @@
+case = int(input())
+for _ in range(case):
+    s = input().lower()
+    tmp = [1] * 26
+    for i in s:
+        if ord(i) > 96 and ord(i) < 123: tmp[ord(i) - 97] = 0
+    if sum(tmp) == 0:
+        print("pangram")
+    else:
+        print("missing ", end='')
+        for i in range(len(tmp)):
+            if tmp[i] == 1: print(chr(i + 97), end='')
+        print()
