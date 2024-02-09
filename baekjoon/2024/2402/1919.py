151f65a733008446be70326294d4cc720b330a95
dhwpdnr <ink1237@gmail.com>
2024-02-09 18:06:19 +0900

    240200

---
+++ baekjoon/2024/2402/1919.py
@@ -0,0 +1,15 @@
+lst = [0]*26
+
+for i in list(input()):
+    lst[ord(i) - 97] += 1
+for j in list(input()):
+    lst[ord(j) - 97] -= 1
+
+print(sum(map(abs, lst)))
+
+a = list(input())
+b = list(input())
+a1, b1 = a[:], b[:]
+out1 = [x for x in a if not x in b1 or b1.remove(x)]
+out2 = [x for x in b if not x in a1 or a1.remove(x)]
+print(len(out1)+len(out2))
