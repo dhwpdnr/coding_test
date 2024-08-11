4785d8e0dbbe76e4a586ef847ba8ae6a485b240d
dhwpdnr <ink1237@gmail.com>
2024-08-11 19:06:38 +0900

    240811:DP

---
+++ programmers/2024/2408/240811.py
@@ -0,0 +1,32 @@
+# 프로그래머스
+# 코딩테스트 연습 > 동적계획법(Dynamic Programming) > N으로 표현
+# https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+def solution(N, number):
+    s = [set() for x in range(8)]
+    for i, x in enumerate(s, start=1):
+        x.add(int(str(N) * i))
+    for i in range(len(s)):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i - j - 1]:
+                    s[i].add(op1 + op2)
+                    s[i].add(op1 - op2)
+                    s[i].add(op1 * op2)
+                    if op2 != 0:
+                        s[i].add(op1 // op2)
+        if number in s[i]:
+            answer = i + 1
+            break
+        else:
+            answer = -1
+    return answer
+
+
+q = solution(5, 12)
+assert q == 4
+print(q)
+
+q = solution(2, 11)
+assert q == 3
+print(q)
