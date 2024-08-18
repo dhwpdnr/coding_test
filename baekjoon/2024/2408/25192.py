896acdca7a46a4f337e22c1e1dcab5f50d0a76d3
dhwpdnr <ink1237@gmail.com>
2024-08-18 22:59:06 +0900

    240818: hash

---
+++ baekjoon/2024/2408/25192.py
@@ -0,0 +1,21 @@
+N = int(input())
+enter = str(input())
+cnt = 0
+dic = {}
+
+for i in range(1, N):
+    word = str(input())
+    if word == "ENTER":
+        for key, value in dic.items():
+            if value == 1:
+                cnt += 1
+        dic = {}
+        continue
+    if word not in dic:
+        dic[word] = 1
+
+for key, value in dic.items():
+    if value == 1:
+        cnt += 1
+
+print(cnt)
